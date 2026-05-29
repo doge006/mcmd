f77b050d445aebf6fa5ed093ed241da7a3ca6fb8
doge <dogebuns5@gmail.com>
2026-05-29 14:36:03 -0500

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let amount = std::env::args().nth(1).expect("no amount given");
+    let amount: i32 = amount.parse().unwrap();
+    use std::process::Command;
+    for _ in 0..amount {
+        Command::new("cmd")
+        .args(["/C", "start", "cmd"])
+        .spawn()
+        .unwrap();
+}
+}
